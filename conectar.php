2302d1d400575ab53d1723f0c25f279807661cbe
Marcio Oliveira <marciolea@gmail.com>
2022-02-18 11:17:34 -0300

    Configuração do banco de dados SQlite

--- conectar.php
+++ conectar.php
@@ -5,7 +5,8 @@
     $con = null;
 
     try {
-        $con = new PDO("mysql:host=localhost;dbname=cadastro", "root", "");
+        //$con = new PDO("mysql:host=localhost;dbname=cadastro", "root", "");
+        $con = new PDO('sqlite:cadastro');
     } catch (PDOException $e) {
         echo $e->getMessage();
         exit();
